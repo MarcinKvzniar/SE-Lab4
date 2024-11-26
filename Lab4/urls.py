eb28359984b1144125d7e5cc3d3af7b2efe62def
MarcinKvzniar <276072@student.pwr.edu.pl>
2024-11-26 16:49:19 +0100

    List 6 - Part 3

--- Lab4/urls.py
+++ Lab4/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet, OrderViewSet, CustomerViewSet
+from .views import ProductViewSet, OrderViewSet, CustomerViewSet, ProductListView, ProductDetailView, ProductCreateView
 
 router = DefaultRouter()
 router.register('products', ProductViewSet, basename='product')
 router.register('orders', OrderViewSet, basename='order')
 router.register('customers', CustomerViewSet, basename='customer')
 
-
 urlpatterns = [
      path('api/', include(router.urls)),
+     path('user/products/', ProductListView.as_view(), name='product_list'),
+     path('user/products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+     path('user/products/new/', ProductCreateView.as_view(), name='product_create'),
 ]
