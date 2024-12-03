eb690615b4c230b2313ae18ee61e96d1526270d0
MarcinKvzniar <276072@student.pwr.edu.pl>
2024-12-03 11:44:02 +0100

    Lab 7 tasks

--- Lab4/urls.py
+++ Lab4/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet, OrderViewSet, CustomerViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 router = DefaultRouter()
 router.register('products', ProductViewSet, basename='product')
@@ -10,4 +12,6 @@ router.register('customers', CustomerViewSet, basename='customer')
 
 urlpatterns = [
      path('api/', include(router.urls)),
+     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
