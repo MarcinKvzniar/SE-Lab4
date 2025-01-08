ed306dcab18396f3003e75a2f70b2481df93b92c
MarcinKvzniar <276072@student.pwr.edu.pl>
2025-01-08 12:04:22 +0100

    Lab 9 - Part 3: Search update

--- Lab4/views.py
+++ Lab4/views.py
@@ -5,12 +5,17 @@ from rest_framework.permissions import IsAuthenticated
 from .permissions import IsAdminOrReadOnly
 from django.views.generic import ListView, DetailView, CreateView
 from .forms import ProductForm
+from rest_framework.filters import SearchFilter
 
 class ProductViewSet(viewsets.ModelViewSet):
   permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
 
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
+
+  filter_backends = (SearchFilter,)
+  search_fields = ['name']
+
 
 class CustomerViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
